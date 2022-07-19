c5c6c20ea5afb9474ae47d90adce15fba12e7c4f
Alisterj <lisarsiss@gmail.com>
2022-07-19 15:23:16 +0300

    Design for myPost - changed

--- src/components/Content/Home/MyPosts/MyPost/MyPost.tsx
+++ src/components/Content/Home/MyPosts/MyPost/MyPost.tsx
@@ -3,13 +3,13 @@ import s from './MyPost.module.css'
 
 const MyPost = () => {
     return (
-        <div className={s.add_post_wrapper}>
-            <div className={s.add_post}>
-                My post
-                <div className={s.add_post_form}>
-                    <input/>
-                    <button className={s.newPost_btn}>Add post</button>
-                    <button className={s.newPost_btn}>Remove</button>
+        <div className={s.add_post}>
+            <span className={s.add_post_span}>My post</span>
+            <div className={s.add_post_form}>
+                <input className={s.add_post_form_input}/>
+                <div className={s.add_post_form_btns}>
+                    <button className={s.btn_add}>Add post</button>
+                    <button className={s.btn_remove}>Remove</button>
                 </div>
             </div>
         </div>
